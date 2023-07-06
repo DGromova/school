a90a24fe7a7f65e973e8c93ac6cd505619f4749f
Daria Gromova <igromova81@gmail.com>
2023-07-06 10:13:17 +0300

    Hw

--- src/main/java/ru/hogwarts/school/controller/FacultyController.java
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.service.FacultyServiceImpl;
 
+import java.util.Collection;
 import java.util.List;
 
 @RestController
@@ -18,13 +19,13 @@ public class FacultyController {
     }
 
     @PostMapping
-    public ResponseEntity createFaculty(@RequestBody Faculty faculty) {
+    public ResponseEntity<Faculty> createFaculty(@RequestBody Faculty faculty) {
         Faculty createdfaculty = facultyServiceImpl.createFaculty(faculty);
         return ResponseEntity.ok(createdfaculty);
     }
 
-    @GetMapping("{studentId}")
-    public ResponseEntity getFaculty(@PathVariable Long facultyId) {
+    @GetMapping("{facultyId}")
+    public ResponseEntity<Faculty> getFaculty(@PathVariable Long facultyId) {
         Faculty faculty = facultyServiceImpl.getFacultyById(facultyId);
         if(faculty == null) {
             return ResponseEntity.notFound() .build();
@@ -33,7 +34,7 @@ public class FacultyController {
     }
 
     @GetMapping("{color}")
-    public ResponseEntity getFacultyByColor(@PathVariable String color) {
+    public ResponseEntity<Collection> getFacultyByColor(@PathVariable String color) {
         List faculty = facultyServiceImpl.getFacultyByColor(color);
         if(faculty.isEmpty()) {
             return ResponseEntity.notFound() .build();
@@ -42,13 +43,13 @@ public class FacultyController {
     }
 
     @PutMapping()
-    public ResponseEntity updateFaculty(@RequestBody Faculty faculty) {
+    public ResponseEntity<Faculty> updateFaculty(@RequestBody Faculty faculty) {
         Faculty updatedFaculty = facultyServiceImpl.updateFaculty(faculty.getId(), faculty);
         return ResponseEntity.ok(updatedFaculty);
     }
 
-    @DeleteMapping("{studentId}")
-    public ResponseEntity deleteFaculty(@PathVariable Long facultyId) {
+    @DeleteMapping("{facultyId}")
+    public ResponseEntity<Faculty> deleteFaculty(@PathVariable Long facultyId) {
         Faculty deletedFaculty = facultyServiceImpl.deleteFaculty(facultyId);
         if(deletedFaculty == null) {
             return ResponseEntity.notFound() .build();
--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.service.StudentServiceImpl;
 
+import java.util.Collection;
 import java.util.List;
 
 @RestController
@@ -18,13 +19,13 @@ public class StudentController {
     }
 
     @PostMapping
-    public ResponseEntity createStudent(@RequestBody Student student) {
+    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
         Student createdStudent = studentServiceImpl.createStudent(student);
         return ResponseEntity.ok(createdStudent);
     }
 
     @GetMapping("{studentId}")
-    public ResponseEntity getStudent(@PathVariable Long studentId) {
+    public ResponseEntity<Student> getStudent(@PathVariable Long studentId) {
         Student student = studentServiceImpl.getStudentById(studentId);
         if(student == null) {
             return ResponseEntity.notFound() .build();
@@ -33,7 +34,7 @@ public class StudentController {
     }
 
     @GetMapping("{studentsAge}")
-    public ResponseEntity getStudentsByAge(@PathVariable int studentsAge) {
+    public ResponseEntity<Collection> getStudentsByAge(@PathVariable int studentsAge) {
         List<Student> studentsByAge = studentServiceImpl.getStudentsByAge(studentsAge);
         if(studentsByAge.isEmpty()) {
             return ResponseEntity.notFound() .build();
@@ -42,13 +43,13 @@ public class StudentController {
     }
 
     @PutMapping()
-    public ResponseEntity updateStudent(@RequestBody Student student) {
+    public ResponseEntity<Student> updateStudent(@RequestBody Student student) {
         Student updatedStudent = studentServiceImpl.updateStudent(student.getId(), student);
         return ResponseEntity.ok(updatedStudent);
     }
 
     @DeleteMapping("{studentId}")
-    public ResponseEntity deleteStudent(@PathVariable Long studentId) {
+    public ResponseEntity<Student> deleteStudent(@PathVariable Long studentId) {
         Student deletedStudent = studentServiceImpl.deleteStudent(studentId);
         if(deletedStudent == null) {
             return ResponseEntity.notFound() .build();
